261ca6a28ac28984bbe0a9d7347a301a054a53e2
Ritika Rawat <Ritikarawat220@gmail.com>
2023-04-06 18:19:38 +0530

    Display a button that checks for a status in categories.js

---
+++ src/components/Categories.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const categories = () => {
+    <div className="categories">
+        <div className="text">
+            <button>Check Status</button>
+        </div>
+    </div>
+};
+
+export default categories;
