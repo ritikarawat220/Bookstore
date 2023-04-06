8552a1e985a50c85a0934027a79da6675d4cf860
Ritika Rawat <Ritikarawat220@gmail.com>
2023-04-06 18:12:50 +0530

    Add Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () => {
+    <main>
+        <h1>BOOKSTORE</h1>
+        <ul>
+            <li>
+                <link to="/">BOOKS</link>
+            </li>
+            <li>
+                <link to="/categories">CATEGORIES</link>
+            </li>
+        </ul>
+    </main>
+};
+
+export default Navbar;
