3e18fbfad01488c5c11c09ea5670a14187f48620
Ritika Rawat <Ritikarawat220@gmail.com>
2023-04-06 18:16:20 +0530

    Update Books.js

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,1 +1,12 @@
 import React from "react";
+import AddBook from "./AddBooks";
+import BookList from "./BookList";
+ 
+const Books = () => {
+    <>
+    <AddBook />
+    <BookList/>
+  </>
+};
+
+export default Books;
