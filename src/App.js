ec879b7b661e6adce2708bbe2b8dd6efca274891
Ritika Rawat <Ritikarawat220@gmail.com>
2023-04-07 15:56:30 +0530

    Add react-router-dome

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import Navbar from './components/Navbar';
 import Books from './components/Books';
 import Categories from './components/Categories';
 
-const App = () => {
-  <Router>
-    <div>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Books />} />
-        <Route path="/Categories" element={<Categories />} />
-      </Routes>
-    </div>
-  </Router>;
-};
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="/Categories" element={<Categories />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
 
 export default App;
