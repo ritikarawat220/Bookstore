0380c0d857edf44b008008b620ca77b8813f123d
Ritika Rawat <Ritikarawat220@gmail.com>
2023-04-11 14:05:41 +0530

    Update AddBook.js

--- src/components/AddBooks.js
+++ src/components/AddBooks.js
@@ -1,16 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/booksSlice';
 
 const AddBook = () => {
-  <div className="add_book">
-    <h1>ADD NEW BOOK</h1>
-    <div>
-      <form>
-        <input type="text" name="title" placeholder="Book Title" />
-        <input type="text" name="author" placeholder="Author" />
-        <button type="submit">Add Book</button>
-      </form>
+  const [newState, setnewState] = useState({
+    id: '',
+    title: '',
+    author: '',
+  });
+
+  const arr = useSelector((state) => state.books.books);
+  const id = arr.length;
+  const onChange = (event) => {
+    setnewState({
+      ...newState,
+      id: (id + 1).toString(),
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const dispatch = useDispatch();
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    dispatch(addBook(newState));
+    setnewState({
+      id: '',
+      title: '',
+      author: '',
+    });
+  };
+
+  return (
+    <div className="add_book">
+      <h1>ADD NEW BOOK</h1>
+      <div>
+        <form onSubmit={onSubmitHandler}>
+          <input
+            type="text"
+            name="title"
+            placeholder="Book Title"
+            required
+            value={newState.title}
+            onChange={onChange}
+          />
+
+          <input
+            type="text"
+            name="author"
+            placeholder="Author"
+            required
+            value={newState.author}
+            onChange={onChange}
+          />
+          <button type="submit">ADD BOOK</button>
+        </form>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default AddBook;
