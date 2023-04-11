894e85680e11a166b3dad9d5dbff5ca005d00ec5
Ritika Rawat <Ritikarawat220@gmail.com>
2023-04-11 14:10:25 +0530

    Update BookDisplay

--- src/components/BookDisplay.js
+++ src/components/BookDisplay.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/booksSlice';
 
 const BookDisplay = (props) => {
-  const { Title, Author } = props;
+  const { Title, Author, Id } = props;
+
+  const dispatch = useDispatch();
+  const handleRemove = () => {
+    dispatch(removeBook(Id));
+  };
+
   return (
     <li className="item">
       <h2>{Title}</h2>
       <h3>{Author}</h3>
-      <button type="submit" className="Remove Text-Style-8">Remove</button>
+      <button type="submit" className="Remove Text-Style-8" onClick={handleRemove}>Remove</button>
     </li>
   );
 };
@@ -15,6 +23,7 @@ const BookDisplay = (props) => {
 BookDisplay.propTypes = {
   Title: PropTypes.string.isRequired,
   Author: PropTypes.string.isRequired,
+  Id: PropTypes.string.isRequired,
 };
 
 export default BookDisplay;
