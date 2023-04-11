5cb98cc4f41a63a7050ac4488ebbcbbcd3852081
Ritika Rawat <Ritikarawat220@gmail.com>
2023-04-11 14:12:55 +0530

    Update BookList

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import BookDisplay from './BookDisplay';
+import { useSelector } from 'react-redux';
+
+
 
 const BookList = () => {
-  const books = [
-    {
-      id: 1,
-      Title: 'The Hunger Games',
-      Author: 'Suzanne-Collins',
-    },
-  ];
+  const books = useSelector((state) => state.books);
   return (
     <>
       <div className="book_content">
         <ul>
-          {books.map((book) => (
+          {books.books.map((book) => (
             <BookDisplay
               Title={book.Title}
               Author={book.Author}
               key={book.id}
+              Id={book.id}
             />
           ))}
         </ul>
