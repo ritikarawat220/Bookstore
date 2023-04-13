4f24018bfb2ac5277a1d029ed69bb6bf53705d41
Ritika Rawat <Ritikarawat220@gmail.com>
2023-04-13 22:22:55 +0530

    Update styling

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -30,49 +30,9 @@ const BookList = () => {
                 category={books.books[key][0].category}
                 id={key}
               />
-              <hr />
             </li>
           ))}
         </ul>
-
-        <div className="book-right">
-          <div className="perc-left">
-            <div className="circle-left">
-              <div className="circle-outer">
-                <div className="circle-inner" />
-              </div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                version="1.1"
-                width="160px"
-                height="160px"
-              >
-                <defs>
-                  <linearGradient id="GradientColor">
-                    <stop offset="0%" stopColor="#e91e63" />
-                    <stop offset="100%" stopColor="#673ab7" />
-                  </linearGradient>
-                </defs>
-                <circle cx="42" cy="50" r="36" strokeLinecap="round" />
-              </svg>
-            </div>
-            <div className="circle-right">
-              <p className="pers">77%</p>
-              <p className="comp">Completed</p>
-            </div>
-          </div>
-          <div className="perc-right">
-            <div className="perc-rightin">
-              <h4>CURRENT CHAPTER</h4>
-              <p>Chapter 17</p>
-              <div>
-                <button type="button">UPDATE PROGRESS</button>
-                {' '}
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="divider" />
         <AddBooks />
       </div>
 
