446613cd3e0eb89c357898600edf03a8df09ff67
Ritika Rawat <Ritikarawat220@gmail.com>
2023-04-13 20:35:46 +0530

    Add styling in update progress

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+// import { CircularProgressbar } from 'react-circular-progressbar';
 import BookDisplay from './BookDisplay';
 import { fetchBooks } from '../redux/books/booksSlice';
+import './BookList.css';
 
 const BookList = () => {
   const dispatch = useDispatch();
@@ -16,9 +18,10 @@ const BookList = () => {
   }, [bookStatus, dispatch]);
 
   return (
-    <div className="book_content">
-      <ul className="list">
-        {bookStatus === 'succeeded'
+    <>
+      <div className="book_content">
+        <ul className="list">
+          {bookStatus === 'succeeded'
           && Object.keys(books.books).map((key) => (
             <li key={key}>
               <BookDisplay
@@ -30,21 +33,55 @@ const BookList = () => {
               <hr />
             </li>
           ))}
-      </ul>
-      <div>
-        <div>
-          <p>85%</p>
-          <p>Completed</p>
+        </ul>
+        <div className="book-butts">
+          <button type="button" className="butt">
+            Comments
+          </button>
+          <button type="button" className="butt">
+            Edit
+          </button>
+        </div>
+
+        <div className="book-right">
+          <div className="perc-left">
+            <div className="circle-left">
+              <div className="circle-outer">
+                <div className="circle-inner" />
+              </div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                version="1.1"
+                width="160px"
+                height="160px"
+              >
+                <defs>
+                  <linearGradient id="GradientColor">
+                    <stop offset="0%" stopColor="#e91e63" />
+                    <stop offset="100%" stopColor="#673ab7" />
+                  </linearGradient>
+                </defs>
+                <circle cx="42" cy="50" r="36" strokeLinecap="round" />
+              </svg>
+            </div>
+            <div className="circle-right">
+              <p className="pers">77%</p>
+              <p className="comp">Completed</p>
+            </div>
+          </div>
+          <div className="perc-right">
+            <div className="perc-rightin">
+              <h4>CURRENT CHAPTER</h4>
+              <p>Chapter 10</p>
+              <div>
+                <button type="button">UPDATE PROGRESS</button>
+                {' '}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <div>
-        <h4>CURRENT CHAPTER</h4>
-        <p>Chapter 3: “Learning”</p>
-        <div>
-          <button type="button">UPDATE PROGRESS</button>
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
 
